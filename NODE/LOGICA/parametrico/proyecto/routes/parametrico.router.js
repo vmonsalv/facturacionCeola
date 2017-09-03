8ce98d38f1b7ca06ae38ab12ae11e5fc049a27fd
vmonsalv <vmonsalv.vm@gmail.com>
2017-09-03 13:40:09 -0300

    cambios en servicio parametrico, crud de protesis, insumos, medicamentos y pabellón

--- NODE/LOGICA/parametrico/proyecto/routes/parametrico.router.js
+++ NODE/LOGICA/parametrico/proyecto/routes/parametrico.router.js
@@ -13,15 +13,37 @@ router.post('/updateEstadoMenu', presupuestoService.updateEstadoMenu);
 
 /* protesis */
 router.post('/getProtesis', presupuestoService.getProtesis);
+router.post('/newProtesis', pacienteService.newProtesis);
+router.post('/deleteProtesis', pacienteService.deleteProtesis);
+router.post('/updateProtesis', pacienteService.updateProtesis);
+router.post('/getProte', pacienteService.getProte);
 
 /* insumos */
 router.post('/getInsumos', presupuestoService.getInsumos);
+router.post('/newInsumo', pacienteService.newInsumo);
+router.post('/deleteInsumo', pacienteService.deleteInsumo);
+router.post('/updateInsumo', pacienteService.updateInsumo);
+router.post('/getInsumo', pacienteService.getInsumo);
 
 /* medicamentos */
 router.post('/getMedicamentos', presupuestoService.getMedicamentos);
+router.post('/newMedicamento', pacienteService.newMedicamento);
+router.post('/deleteMedicamento', pacienteService.deleteMedicamento);
+router.post('/updateMedicamento', pacienteService.updateMedicamento);
+router.post('/getMedicamento', pacienteService.getMedicamento);
+
+/* Pabellón */
+router.post('/getPabellones', presupuestoService.getPabellones);
+router.post('/newPabellon', pacienteService.newPabellon);
+router.post('/deletePabellon', pacienteService.deletePabellon);
+router.post('/updatePabellon', pacienteService.updatePabellon);
+router.post('/getPabellon', pacienteService.getPabellon);
 
 /* Equipo medico */
 router.post('/getEquipoMedico', presupuestoService.getEquipoMedico);
-
+/*router.post('/newFuncionarioEM', pacienteService.newFuncionarioEM);
+router.post('/deleteFuncionarioEM', pacienteService.deleteFuncionarioEM);
+router.post('/updateFuncionarioEM', pacienteService.updateFuncionarioEM);
+router.post('/getFuncionarioEM', pacienteService.getFuncionarioEM);*/
 
 module.exports = router;
